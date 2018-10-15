83f06b4f1fe01c32a73d4a9c67d103a5deea0495
kejiqing@gmail.com <jiqing.kejq@antfin.com>
2018-10-15 20:28:44 +0800

    update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='pandasplit',
-      version='0.3',
+      version='0.4',
       description='split pandas data into groups',
       url='http://github.com/passionke/pandasplit',
       author='passionke',
